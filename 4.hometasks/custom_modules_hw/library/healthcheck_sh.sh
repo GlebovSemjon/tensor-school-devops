d499fd4e31509c7c32955b9a54d27f326a7035d9
GlebovSemjon <Schir88@gmail.com>
2022-03-20 02:43:18 +0500

    fix failed tasks

--- 4.hometasks/custom_modules_hw/library/healthcheck_sh.sh
+++ 4.hometasks/custom_modules_hw/library/healthcheck_sh.sh
@@ -7,20 +7,20 @@ tls=$(cat $1 | grep -Po '(?<="tls": )(.*?)(?=,)')
 function errorcode_select {
     if echo $code | grep 'HTTP'; then
         if echo $code | grep 200; then
-            echo "{\"status\": \"$code Ok\", \"site\": \"$addr\", \"tls\": \"$tls\"}"
+            echo "{\"failed\": false, \"status\": \"$code\", \"site\": \"$addr\", \"tls\": $tls}"
         elif echo $code | grep 1[0-9][0-9]; then
-            echo "{\"status\": \"$code Informational\", \"site\": \"$addr\", \"tls\": \"$tls\"}"
+            echo "{\"failed\": true, \"status\": \"$code Informational\", \"site\": \"$addr\", \"tls\": $tls}"
         elif echo $code | grep 2[0-9][0-9]; then
-            echo "{\"status\": \"$code Success\", \"site\": \"$addr\", \"tls\": \"$tls\"}"
+            echo "{\"failed\": true, \"status\": \"$code Success\", \"site\": \"$addr\", \"tls\": $tls}"
         elif echo $code | grep 3[0-9][0-9]; then
-            echo "{\"status\": \"$code Redirection\", \"site\": \"$addr\", \"tls\": \"$tls\"}"
+            echo "{\"failed\": true, \"status\": \"$code Redirection\", \"site\": \"$addr\", \"tls\": $tls}"
         elif echo $code | grep 4[0-9][0-9]; then
-            echo "{\"status\": \"$code Client Error\", \"site\": \"$addr\", \"tls\": \"$tls\"}"
+            echo "{\"failed\": true, \"status\": \"$code Client Error\", \"site\": \"$addr\", \"tls\": $tls}"
         elif echo $code | grep 5[0-9][0-9]; then
-            echo "{\"status\": \"$code Server Error\", \"site\": \"$addr\", \"tls\": \"$tls\"}"
+            echo "{\"failed\": true, \"status\": \"$code Server Error\", \"site\": \"$addr\", \"tls\": $tls}"
         fi
     else
-        echo "{\"status\": \"site not found\", \"site\": \"$addr\", \"tls\": \"$tls\"}"
+        echo "{\"failed\": true, \"status\": \"site not found\", \"site\": \"$addr\", \"tls\": $tls}"
     fi
 }
 
@@ -28,6 +28,6 @@ if [[ $tls = 'true' ]]; then
     code=$(curl -Isk -m 5 https://$addr | grep HTTP | tr -d '\r\n')
     errorcode_select
 else
-    code=$(curl -Isk -m 5 https://$addr | grep HTTP | tr -d '\r\n')
+    code=$(curl -Is -m 5 http://$addr | grep HTTP | tr -d '\r\n')
     errorcode_select
 fi
