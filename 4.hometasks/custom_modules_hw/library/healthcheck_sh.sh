94883afefd90abe5832240d447b5d1970ae84a49
GlebovSemjon <Schir88@gmail.com>
2022-03-20 10:56:52 +0500

    fix msg error

--- 4.hometasks/custom_modules_hw/library/healthcheck_sh.sh
+++ 4.hometasks/custom_modules_hw/library/healthcheck_sh.sh
@@ -7,20 +7,20 @@ tls=$(cat $1 | grep -Po '(?<="tls": )(.*?)(?=,)')
 function errorcode_select {
     if echo $code | grep 'HTTP'; then
         if echo $code | grep 200; then
-            echo "{\"failed\": false, \"status\": \"$code\", \"site\": \"$addr\", \"tls\": $tls}"
+            echo "{\"failed\": false, \"status\": \"$code\", \"msg\": \"Ok\", \"site\": \"$addr\", \"tls\": $tls}"
         elif echo $code | grep 1[0-9][0-9]; then
-            echo "{\"failed\": true, \"status\": \"$code Informational\", \"site\": \"$addr\", \"tls\": $tls}"
+            echo "{\"failed\": true, \"status\": \"$code\", \"msg\": \"Informational\", \"site\": \"$addr\", \"tls\": $tls}"
         elif echo $code | grep 2[0-9][0-9]; then
-            echo "{\"failed\": true, \"status\": \"$code Success\", \"site\": \"$addr\", \"tls\": $tls}"
+            echo "{\"failed\": true, \"status\": \"$code\", \"msg\": \"Success\", \"site\": \"$addr\", \"tls\": $tls}"
         elif echo $code | grep 3[0-9][0-9]; then
-            echo "{\"failed\": true, \"status\": \"$code Redirection\", \"site\": \"$addr\", \"tls\": $tls}"
+            echo "{\"failed\": true, \"status\": \"$code\", \"msg\": \"Redirection\", \"site\": \"$addr\", \"tls\": $tls}"
         elif echo $code | grep 4[0-9][0-9]; then
-            echo "{\"failed\": true, \"status\": \"$code Client Error\", \"site\": \"$addr\", \"tls\": $tls}"
+            echo "{\"failed\": true, \"status\": \"$code\", \"msg\": \"Client Error\", \"site\": \"$addr\", \"tls\": $tls}"
         elif echo $code | grep 5[0-9][0-9]; then
-            echo "{\"failed\": true, \"status\": \"$code Server Error\", \"site\": \"$addr\", \"tls\": $tls}"
+            echo "{\"failed\": true, \"status\": \"$code\", \"msg\": \"Server Error\", \"site\": \"$addr\", \"tls\": $tls}"
         fi
     else
-        echo "{\"failed\": true, \"status\": \"site not found\", \"site\": \"$addr\", \"tls\": $tls}"
+        echo "{\"failed\": true, \"msg\": \"site not found\", \"site\": \"$addr\", \"tls\": $tls}"
     fi
 }
 
