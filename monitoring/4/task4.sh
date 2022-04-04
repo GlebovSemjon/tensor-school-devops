80eff796dd2cddc8c6eddc9518c3927471e2b70d
GlebovSemjon <Schir88@gmail.com>
2022-04-04 13:45:42 +0500

    fix task4.sh

--- monitoring/4/task4.sh
+++ monitoring/4/task4.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
 TS=$(date +%s%N)
 LA=$(cat /proc/loadavg)
-echo loadavg,task=4 LA1=${LA:0:4},LA5=${LA:5:4},LA15=${LA:10:4} $TS
+LA1=$( echo $LA | cut -d ' ' -f 1 )
+LA5=$( echo $LA | cut -d ' ' -f 2 )
+LA15=$( echo $LA | cut -d ' ' -f 3 )
+
+echo loadavg,task=4 LA1=$LA1,LA5=$LA5,LA15=$LA15 $TS
